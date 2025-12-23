a858a5db98b5a3932bf5fac780dad2e2756cc58c
Jenisha Shah <jenishashah1607@gmail.com>
2025-12-23 21:45:15 +0530

    Deploy correct Vite build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   base: '/retirement-savings-simulator/',
   build: {
-    outDir: 'dist'
+    outDir: 'docs',
+    emptyOutDir: true
   }
 })
+
 
 
 
